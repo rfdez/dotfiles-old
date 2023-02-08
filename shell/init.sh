d8a62170508c950fc3f88c44a5bac14729474409
Raúl Fernández Fernández <r.fernandez@flexiw.com>
2023-02-08 20:28:35 +0100

    chore(shell): export private
    Signed-off-by: Raúl Fernández Fernández <r.fernandez@flexiw.com>

--- shell/init.sh
+++ shell/init.sh
@@ -1,9 +1,5 @@
 # This is a useful file to have the same aliases/functions in bash and zsh
-
 source "$DOTFILES_PATH/shell/aliases.sh"
 source "$DOTFILES_PATH/shell/exports.sh"
+source "$DOTFILES_PATH/shell/private-exports.sh"
 source "$DOTFILES_PATH/shell/functions.sh"
-
-set -o allexport
-source "$HOME/.env"
-set +o allexport
